01ece2463fb9fecb8a903f61f30304918848b393
Nutcha Romjun <soderdahl.nutcha@gmail.com>
2020-01-02 09:47:43 +0100

    init commit

---
+++ src/test/java/webshopapp/webshopapp/WebshopappApplicationTests.java
@@ -0,0 +1,13 @@
+package webshopapp.webshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
